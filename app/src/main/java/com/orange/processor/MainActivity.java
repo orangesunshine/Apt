d1add3e28e9e7e2d255fb96b741a82a6dab6829d
429360705@qq.com <429360705@qq.com>
2019-03-14 17:40:35 +0800

    first commnit

---
+++ app/src/main/java/com/orange/processor/MainActivity.java
@@ -0,0 +1,28 @@
+package com.orange.processor;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.orange.annotation.BindView;
+import com.orange.apt.BindViewTools;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tv_test)
+    TextView tvTest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BindViewTools.bind(this);
+        tvTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "onClick", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
---
+++ processor/src/main/java/com/orange/processor/BindViewProcessor.java
@@ -0,0 +1,110 @@
+package com.orange.processor;
+
+import com.google.auto.service.AutoService;
+import com.orange.annotation.BindView;
+import com.squareup.javapoet.JavaFile;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.util.Elements;
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+
+@AutoService(Processor.class)
+public class BindViewProcessor extends AbstractProcessor {
+    private Messager mMessager;
+    private Elements mElementUtils;
+    private Map<String, ClassCreatorProxy> mProxyMap = new HashMap<>();
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        mMessager = processingEnv.getMessager();
+        mElementUtils = processingEnv.getElementUtils();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        HashSet<String> supportTypes = new LinkedHashSet<>();
+        supportTypes.add(BindView.class.getCanonicalName());
+        return supportTypes;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnv) {
+        mMessager.printMessage(Diagnostic.Kind.NOTE, "processing...");
+        mProxyMap.clear();
+        //得到所有的注解
+        Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(BindView.class);
+        for (Element element : elements) {
+            VariableElement variableElement = (VariableElement) element;
+            TypeElement classElement = (TypeElement) variableElement.getEnclosingElement();
+            String fullClassName = classElement.getQualifiedName().toString();
+            ClassCreatorProxy proxy = mProxyMap.get(fullClassName);
+            if (proxy == null) {
+                proxy = new ClassCreatorProxy(mElementUtils, classElement);
+                mProxyMap.put(fullClassName, proxy);
+            }
+            BindView bindAnnotation = variableElement.getAnnotation(BindView.class);
+            int id = bindAnnotation.value();
+            proxy.putElement(id, variableElement);
+        }
+        return processByJavaPoet();
+    }
+
+    private boolean processByJavaPoet() {
+        //通过javapoet生成
+        for (String key : mProxyMap.keySet()) {
+            ClassCreatorProxy proxyInfo = mProxyMap.get(key);
+            JavaFile javaFile = JavaFile.builder(proxyInfo.getPackageName(), proxyInfo.generateJavaCode2()).build();
+            try {
+                //　生成文件
+                javaFile.writeTo(processingEnv.getFiler());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        mMessager.printMessage(Diagnostic.Kind.NOTE, "process finish ...");
+        return true;
+    }
+
+    private boolean processByGenerateCode(RoundEnvironment roundEnv) {
+        //通过遍历mProxyMap，创建java文件
+        for (String key : mProxyMap.keySet()) {
+            ClassCreatorProxy proxyInfo = mProxyMap.get(key);
+            try {
+                mMessager.printMessage(Diagnostic.Kind.NOTE, " --> create " + proxyInfo.getProxyClassFullName());
+                JavaFileObject jfo = processingEnv.getFiler().createSourceFile(proxyInfo.getProxyClassFullName(), proxyInfo.getTypeElement());
+                Writer writer = jfo.openWriter();
+                writer.write(proxyInfo.generateJavaCode());
+                writer.flush();
+                writer.close();
+            } catch (IOException e) {
+                mMessager.printMessage(Diagnostic.Kind.NOTE, " --> create " + proxyInfo.getProxyClassFullName() + "error");
+            }
+        }
+
+        mMessager.printMessage(Diagnostic.Kind.NOTE, "process finish ...");
+        return true;
+    }
+}
